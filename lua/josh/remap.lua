eb56e612ae55715399b6d20097e6c1f659b3d65d
MindfulLearner <dimajosh1009@gmail.com>
2024-07-19 05:30:31 +0200

    swag

--- lua/josh/remap.lua
+++ lua/josh/remap.lua
@@ -43,7 +43,7 @@ vim.keymap.set("n", "<leader>Y", [["+Y]])
 vim.keymap.set({"n", "v"}, "<leader>d", [["_d]])
 
 -- Sostituisce Ctrl+c con Esc in modalità inserimento.
-vim.keymap.set("i", "<C-c>", "<Esc>")
+-- vim.keymap.set("i", "<C-c>", "<Esc>")
 
 -- Disabilita il comando Q in modalità normale.
 vim.keymap.set("n", "Q", "<nop>")
